f981cc2f8bfdf056da34a3ec94ec7329556173c3
Marvin Kurka <8013@htl.rennweg.at>
2023-10-02 08:41:53 +0200

    added router and worked on calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditWorkoutsComponent } from './edit-workouts/edit-workouts.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "workouts", component: EditWorkoutsComponent },
+  { path: "", component: CalendarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -4,6 +4,8 @@ import { Exercise } from './exercise';
 import { select, Store } from '@ngrx/store';
 import { selectAllExercises, selectExerciseByDate } from './exercise.selector';
 import { ExerciseState } from './exercise.reducer';
+import { Workout } from "../edit-workouts/workout";
+import { selectAllWorkouts } from '../edit-workouts/edit-workouts.selector';
 import { addExercise, removeExercise } from './exercise.actions';
 
 @Component({
@@ -15,22 +17,25 @@ export class CalendarComponent {
 
   exercises$: Observable<Exercise[]>;
 
+  workouts$: Observable<Workout[]>;
+
   public currentExercise: Observable<Exercise | undefined> = new Observable(undefined);
 
-  constructor(private store: Store<ExerciseState>) {
-    this.exercises$ = this.store.pipe(select(selectAllExercises));
+  constructor(private exerciseStore: Store<ExerciseState>, private workoutStore: Store<ExerciseState>) {
+    this.exercises$ = this.exerciseStore.pipe(select(selectAllExercises));
+    this.workouts$ = this.workoutStore.pipe(select(selectAllWorkouts))
   }
 
   selectDay(date: Date | Date[]) {
     if (!(date instanceof Date)) return;
     date as Date;
-    this.currentExercise = this.store.pipe(select(selectExerciseByDate(date)))
+    this.currentExercise = this.exerciseStore.pipe(select(selectExerciseByDate(date)))
     console.log(this.currentExercise);
   }
 
   // private sameDay(d1: Date, d2: Date) {
   //   return d1.getFullYear() === d2.getFullYear() &&
-  //     d1.getMonth() === d2.getMonth() &&
+      // d1.getMonth() === d2.getMonth() &&
   //     d1.getDate() === d2.getDate();
   // }
 
